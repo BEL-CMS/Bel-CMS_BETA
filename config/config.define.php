ac1a054b4ac58f15819b3d49af5b90f796e707a3
DESKTOP-TV0U201\Stive <stivedeterme@msn.com>
2016-12-15 19:45:13 +0100

    + add function GetHost for SSL

--- config/config.define.php
+++ config/config.define.php
@@ -14,8 +14,33 @@ if (!defined('CHECK_INDEX')) {
 	exit(ERROR_INDEX);
 }
 
+class GetHost {
+
+	public static function isHttps() {
+		return (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ||
+			$_SERVER['SERVER_PORT'] == 443;
+	}
+
+	public static function getBaseUrl() {
+		$protocol = self::isHttps() ? 'https' : 'http';
+		if (isset($_SERVER["SERVER_PORT"])) {
+			$port = ':' . $_SERVER["SERVER_PORT"];
+		} else {
+			$port = '';
+		}
+		if ($port == ':80' || $port == ':443') {
+			$port = '';
+		}
+		$uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+		$cutoff = strpos($uri, 'index.php');
+		$uri = substr($uri, 0, $cutoff);
+		$serverName = getenv('HOSTNAME')!==false ? getenv('HOSTNAME') : (isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : '');
+		return "$protocol://{$serverName}$port{$uri}/";	
+	}
+}
+
 $define = array(
-	'BASE_URL'            => 'http://'.$_SERVER['HTTP_HOST'].str_replace('index.php', '', $_SERVER['PHP_SELF']),
+	'BASE_URL'            => GetHost::getBaseUrl(),
 	'ROOT_INDEX'          => str_replace('index.php', '', $_SERVER['PHP_SELF']),
 	'ROOT_CLASS'          => ROOT.'class'.DS,
 	'ROOT_CONFIG'         => ROOT.'config'.DS,
