635910f7d00fe07bba1c674cd1ccaa5ac6580eba
DESKTOP-TV0U201\Stive <stivedeterme@msn.com>
2016-12-17 14:28:23 +0100

    * update management + add page parameter

--- class/management.class.php
+++ class/management.class.php
@@ -76,14 +76,14 @@ class Management
 
 		$this->view = trim(constant('GET_ACTION'));
 		$this->id   = Common::secureRequest(constant('GET_ID'));
-		$this->page = constant('GET_PAGE');
+		$this->page = strtolower(constant('GET_PAGE'));
 
 		$this->return = self::View();
 
 		$this->pageManagement = ROOT_MANAGEMENT.'page'.DS;
 	}
 
-	private function LoginManagement () {
+	private function loginManagement () {
 		if (GET_PAGE == 'login') {
 			if (!empty($_POST['privatemail']) && !empty($_POST['password'])) {
 				$this->jquery = $this->model->login($_POST['privatemail'], $_POST['password']);
@@ -91,7 +91,9 @@ class Management
 		}
 		require ROOT_MANAGEMENT_TPL.'login.php';
 	}
-
+	#####################################
+	# Get CSS / JS / IMG / LANG
+	#####################################
 	private function css ()
 	{
 		return ROOT_MANAGEMENT_TPL.'css'.DS;
@@ -110,20 +112,22 @@ class Management
 			include $file;
 		}
 	}
+	#####################################
+	# Get View and Test
+	#####################################
 	private function View ()
 	{
 		ob_start();
 
 		if ($_SESSION['LOGIN_MANAGEMENT'] !== true) {
-			self::LoginManagement();
+			self::loginManagement();
 		} else {
 			$this->require = GET_PAGE == 'login' ? 'Dashboard' : GET_PAGE;
 			if ($this->require == 'Dashboard') Common::Redirect('Dashboard?Management');
-
-			if ($this->page == 'dashboard' or $this->page == 'error') {
-				self::PageManagement(ROOT_MANAGEMENT.$this->page, true);
-			} else if ($this->page == 'logout') {
-				self::PageLogout();
+			$pageSelf = array('dashboard', 'logout', 'parameter');
+			if (in_array($this->page, $pageSelf)) {
+				$page = $this->page;
+				self::$page();
 			} else {
 				if ($this->ExistsPages($this->page)) {
 					self::PageManagement(ROOT_PAGES.$this->page.DS.$this->dirName);
@@ -162,120 +166,66 @@ class Management
 
 		return $buffer;
 	}
-	private function PageError ($error = '404') {
-		// 1 = no exist page
-		// 2 = no access page
-		// 3 = no existe widgets
-		// 4 = no access widgets
-		// 5 = no exist page or widgets
-		// 6 = no file exist
-		// controller = controller file no exist
-		// dashboard  = error file dashboard
-		// 404 default
-		if ($this->error === true) {
-			ob_start();
-			if ($error == '404') {
-				$title = '404';
-			} else if ($error == 1) {
-				$title = 'The requested page does not exist';
-			} else if ($error == 2) {
-				$title = 'The requested page does not access';
-			} else if ($error == 3) {
-				$title = 'The requested widgets does not exist';
-			} else if ($error == 4) {
-				$title = 'The requested widgets does not access';
-			} else if ($error == 5) {
-				$title = 'The requested widgets or page does not exist';
-			} else if ($error == 6) {
-				$title = 'The requested file does not exist';
-			} else if ($error == 'controller') {
-				$title = 'The requested controller does not exist';
-			} else if ($error == 'dashboard') {
-				$title = 'The requested dashboard does not exist';
-			} else {
-				$title = ERROR_UNKNOWN_MANAGEMENT;
-			}
-			require ROOT_MANAGEMENT_TPL.'error.php';
-			$this->buffer = ob_get_contents();
-			ob_end_clean();
-			self::html();
-			$this->error = false;
-		}
-	}
-
-	private function PageManagement ($d, $m = false)
+	#####################################
+	# Get MVC
+	#####################################
+	private function PageManagement ($d)
 	{
 		ob_start("ob_gzhandler");
 
 		$error = false;
 
-		if ($m) {
-			if (is_file($d.'.php')) {
-				require $d.'.php';
-				$this->buffer = ob_get_contents();
+		$file = $d.DS.$this->view.'.php';
+		if (is_file($file)) {
+			$file_model      = $d.DS.'model.php';
+			$file_controller = $d.DS.'controller.php';
+			if (is_file($file_model)) require $file_model;
+			if (is_file($file_controller)) {
+				include_once $file_controller;
+				$NewClassController = 'ControllerManagement'.ucfirst($this->page);
+				if (class_exists($NewClassController)) {
+					$controller = New $NewClassController($this->id);
+					$methode    = $this->view;
+					if (method_exists($controller, $methode)) {
+						$controller->$methode();
+						if ($controller->error !== null) {
+							if ($controller->error == 1) {
+								self::PageError(1);
+							} else if ($controller->error == 2) {
+								self::PageError(1);
+							} else {
+								self::PageError(0);
+							}
+							$error = true;
+						}
+						$this->data = $controller->data;
+					}
+				}
 			} else {
-				self::PageError('dashboard');
+				$error = true;
+				self::PageError(7);
+			}
+			if ($error === false) {
+				require $file;
+				$this->buffer = ob_get_contents();
 			}
 		} else {
-			$file = $d.DS.$this->view.'.php';
-			if (is_file($file)) {
-				$file_model      = $d.DS.'model.php';
-				$file_controller = $d.DS.'controller.php';
-				if (is_file($file_model)) require $file_model;
-				if (is_file($file_controller)) {
-					include_once $file_controller;
-					$NewClassController = 'ControllerManagement'.ucfirst($this->page);
-					if (class_exists($NewClassController)) {
-						$controller = New $NewClassController($this->id);
-						$methode    = $this->view;
-						if (method_exists($controller, $methode)) {
-							$controller->$methode();
-							if ($controller->error !== null) {
-								if ($controller->error == 1) {
-									self::PageError(1);
-								} else if ($controller->error == 2) {
-									self::PageError(1);
-								} else {
-									self::PageError(0);
-								}
-								$error = true;
-							}
-							$this->data = $controller->data;
-						}
-					}
-				} else {
-					$error = true;
-					self::PageError(7);
-				}
-				if ($error === false) {
-					require $file;
-					$this->buffer = ob_get_contents();
-				}
-			} else {
-				self::PageError(6);
-			}
+			self::PageError(6);
 		}
+
 		ob_end_clean();
 		self::html();
 	}
-
-	private function PageDashboard ()
-	{
-		require ROOT_MANAGEMENT_TPL.'index.php';
-	}
-
-	private function PageLogout ()
-	{
-		$this->jquery = $this->model->logout();
-	}
-
+	#####################################
+	# Main HTML
+	#####################################
 	private function html() {
 		?>
 			<!DOCTYPE html>
 			<html lang="en">
 			<head>
 			<meta charset="utf-8">
-			<title><?=GET_PAGE?> - BEL-CMS - MANAGEMENT</title>
+			<title><?=ucfirst(GET_PAGE)?> - BEL-CMS - MANAGEMENT</title>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 			<meta name="apple-mobile-web-app-capable" content="yes">
 			<link href="/{css}bootstrap.min.css" rel="stylesheet">
@@ -296,7 +246,7 @@ class Management
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</a>
-						<a class="brand" href="dashboard?management">BEL-CMS - MANAGEMENT </a>
+						<a class="brand" href="dashboard?management">BEL-CMS - MANAGEMENT</a>
 						<div class="nav-collapse">
 							<ul class="nav pull-right">
 								<li class="dropdown">
@@ -401,6 +351,9 @@ class Management
 			</html>
 		<?php
 	}
+	#####################################
+	# Scan Page & Widgets / Access
+	#####################################
 	private function ExistsPages($search = false) {
 
 		$scanDir = Common::ScanDirectory(ROOT_PAGES);
@@ -500,5 +453,172 @@ class Management
 		}
 
 		return $access;
+	}
+	#####################################
+	# Error
+	#####################################
+	private function PageError ($error = '404') {
+		// 1 = no exist page
+		// 2 = no access page
+		// 3 = no existe widgets
+		// 4 = no access widgets
+		// 5 = no exist page or widgets
+		// 6 = no file exist
+		// controller = controller file no exist
+		// inner  = error internal file
+		// 404 default
+		if ($this->error === true) {
+			ob_start();
+			if ($error == '404') {
+				$title = '404';
+			} else if ($error == 1) {
+				$title = 'The requested page does not exist';
+			} else if ($error == 2) {
+				$title = 'The requested page does not access';
+			} else if ($error == 3) {
+				$title = 'The requested widgets does not exist';
+			} else if ($error == 4) {
+				$title = 'The requested widgets does not access';
+			} else if ($error == 5) {
+				$title = 'The requested widgets or page does not exist';
+			} else if ($error == 6) {
+				$title = 'The requested file does not exist';
+			} else if ($error == 'controller') {
+				$title = 'The requested controller does not exist';
+			} else if ($error == 'inner') {
+				$title = 'The requested internal page does not exist';
+			} else {
+				$title = ERROR_UNKNOWN_MANAGEMENT;
+			}
+			require ROOT_MANAGEMENT_TPL.'error.php';
+			$this->buffer = ob_get_contents();
+			ob_end_clean();
+			self::html();
+			$this->error = false;
+		}
+	}
+	#####################################
+	# Internal page
+	#####################################
+	private function dashboard()
+	{
+		ob_start("ob_gzhandler");
+
+		if (is_file(ROOT_MANAGEMENT.$this->page.'.php')) {
+			require ROOT_MANAGEMENT.$this->page.'.php';
+			$this->buffer = ob_get_contents();
+		} else {
+			self::PageError('inner');
+		}
+
+		ob_end_clean();
+		self::html();
+	}
+	private function logout ()
+	{
+		$this->jquery = $this->model->logout();
+	}
+	private function parameter()
+	{
+		if (!empty($_POST)) {
+			foreach ($_POST as $k => $v) {
+				if ($k == 8) {
+					$v = implode(',', $v);
+				}
+				$sql = New BDD();
+				$sql->table('TABLE_CONFIG');
+				$sql->where(array('name'=>'id','value'=>$k));
+				$sql->sqlData(array('value' => $v));
+				$sql->update();
+				unset($sql);
+			}
+
+			$save = array(
+				'type' => 'success',
+				'text' => SAVE_BDD_SUCCESS
+			);
+
+			Common::Redirect('Parameter?management', 2);
+
+		} else {
+			$formText     = array('CMS_WEBSITE_NAME', 'CMS_WEBSITE_DESCRIPTION', 'CMS_MAIL_WEBSITE', 'CMS_WEBSITE_KEYWORDS');
+			$formRadio    = array(
+				'CMS_JQUERY'      => array('on', 'off'),
+				'CMS_JQUERY_UI'   => array('on', 'off'),
+				'CMS_BOOTSTRAP'   => array('on', 'off'),
+				'CMS_TPL_WEBSITE' => self::getTpl()
+			);
+			$formCheckbox = array(
+				'CMS_TPL_FULL'    => Common::ScanDirectory(ROOT_PAGES)
+			);
+
+			$sql = New BDD();
+			$sql->table('TABLE_CONFIG');
+			$sql->orderby(array(array('name' => 'name', 'type' => 'DESC')));
+			$sql->queryAll();
+			$return = $sql->data;
+
+			$form  = '';
+
+			foreach ($return as $d) {
+				$input = '';
+				$name  = (defined('ADMIN_'.$d->name)) ? constant('ADMIN_'.$d->name) : $d->name;
+				$help  = (defined('ADMIN_'.$d->name.'_HELP')) ? constant('ADMIN_'.$d->name.'_HELP') : null;
+				if (in_array($d->name, $formText)) {
+					$input = '<input name="'.$d->id.'" type="text" class="span6" id="label_'.$d->id.'" value="'.$d->value.'">';
+				} else if (array_key_exists($d->name, $formRadio)) {
+					foreach ($formRadio[$d->name] as $a) {
+						$checked = $a == $d->value ? 'checked="checked"' : '';
+						$input .= '<label class="radio inline">';
+						$input .= '<input '.$checked.' name="'.$d->id.'" type="radio" id="label_'.$d->id.'" value="'.$a.'">'.$a;
+						$input .= '</label>';
+					}
+				} else if (array_key_exists($d->name, $formCheckbox)) {
+					$value = explode(',', $d->value);
+					foreach ($formCheckbox[$d->name] as $a) {
+						$checked = in_array($a, preg_replace('/\s+/', '', $value)) ? 'checked="checked"' : '';
+						$input .= '<label class="checkbox inline">';
+						$input .= '<input '.$checked.' name="'.$d->id.'[]" type="checkbox" id="label_'.$d->id.'" value="'.$a.'">'.$a;
+						$input .= '</label>';
+					}
+				} else {
+					$input = '<input name="'.$d->id.'" type="text" class="span6" id="label_'.$d->id.'" value="'.$d->value.'">';
+				}
+				$form .= '<div class="control-group">';
+				$form .= '<label class="control-label" for="label_'.$d->id.'">'.$name.'</label>';
+				$form .= '<div class="controls">';
+				$form .= $input;
+				$form .= $help;
+				$form .= '</div>';
+				$form .= '</div>';
+				unset($name,$help, $input);
+			}
+		}
+
+		ob_start("ob_gzhandler");
+
+		if (is_file(ROOT_MANAGEMENT.$this->page.'.php')) {
+			require ROOT_MANAGEMENT.$this->page.'.php';
+			$this->buffer = ob_get_contents();
+		} else {
+			self::PageError('inner');
+		}
+
+		ob_end_clean();
+		self::html();
+	}
+	private function getTpl ()
+	{
+		$return = Common::ScanDirectory(ROOT_TPL);
+
+		if (count($return) !== 0) {
+			foreach ($return as $k => $n) {
+				if (!is_file(ROOT_TPL.$n.DS.'template.php')) {
+					unset($return[$k]);
+				}
+			}
+		}
+
+		return $return;
 	}
 }
--- management/lang.php
+++ management/lang.php
@@ -18,8 +18,26 @@ Common::constant(array(
 	#####################################
 	# Fichier lang en français - Admin
 	#####################################
+	'PARAMETER'                => 'Paramètre',
+	'PARAMETERS'               => 'Paramètres',
 	'ERROR_UNKNOWN_MANAGEMENT' => 'Erreur inconnu',
 	'DASHBOARD'                => 'Tableau de bord',
 	'GUIDELY'                  => 'Guide',
-	'SHOUTBOX'                 => 'T\'chat'
+	'SHOUTBOX'                 => 'T\'chat',
+	'SAVE_BDD_SUCCESS'         => 'La sauvegarde a été effectué avec succès',
+	#####################################
+	# _parameter - Admin
+	#####################################
+	'ADMIN_CMS_WEBSITE_NAME'         => 'Nom',
+	'ADMIN_CMS_TPL_WEBSITE'          => 'Template',
+	'ADMIN_CMS_WEBSITE_DESCRIPTION'  => 'Descripion',
+	'ADMIN_CMS_REGISTER_CHARTER'     => 'Règlement',
+	'ADMIN_CMS_MAIL_WEBSITE'         => 'E-mail Administrateur',
+	'ADMIN_CMS_WEBSITE_KEYWORDS'     => 'Mot Clé',
+	'ADMIN_CMS_WEBSITE_LANG'         => 'Langue du site',
+	'ADMIN_CMS_TPL_FULL'             => 'Page sans widgets (droit / gauche)',
+	'ADMIN_CMS_MAIL'                 => '',
+	'ADMIN_CMS_JQUERY'               => 'jQuery CMS',
+	'ADMIN_CMS_JQUERY_UI'            => 'jQuery UI CMS',
+	'ADMIN_CMS_BOOTSTRAP'            => 'Bootstrap CMS',
 ));
